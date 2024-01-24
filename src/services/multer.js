ab8447dfeb83135f9441d8d12ab60e3ad909fe04
Mohamed Ragab <rmidoragab222@gmail.com>
2024-01-24 18:58:51 +0200

    update multer file

--- src/services/multer.js
+++ src/services/multer.js
@@ -1,30 +1,20 @@
-import multer from "multer";
+import multer from 'multer'
 
-export const multerValidation = {
-  image: ["image/jpeg", "image/png", "image/jif"],
-  pdf: ["application/pdf"],
-};
+export const fileValidation = {
+  image: ['image/png', 'image/jpeg', 'image/jif'],
+  pdf: ['application/pdf'],
+}
 
-export const HME = (err, req, res, next) => {
-  if (err) {
-    res.status(400).json({ message: "Multer error", err });
-  } else {
-    next();
-  }
-};
-export function myMulter(customValidation) {
-  if (!customValidation) {
-    customValidation = multerValidation.image;
-  }
-
-  const storage = multer.diskStorage({});
+export function myMulter(customValidation = fileValidation.image) {
+  const storage = multer.diskStorage({})
   function fileFilter(req, file, cb) {
-    if (customValidation.includes(file.mimetype)) {
-      cb(null, true);
+    if (!customValidation.includes(file.mimetype)) {
+      cb('In-valid format', false)
     } else {
-      cb("In-valid format", false);
+      cb(null, true)
     }
   }
-  const upload = multer({ dest: "upload", fileFilter, storage });
-  return upload;
+
+  const upload = multer({ fileFilter, storage })
+  return upload
 }
